83e1a38343c263376e0a6ff2366f655a5be335aa
aspsnd <2546697613@qq.com>
2021-11-26 23:25:56 +0800

    dev

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,11 @@
-import { readdir } from "fs/promises";
+import { copyFile, readdir, readFile, writeFile } from "fs/promises";
 import chalk from "chalk";
 import path, { resolve } from "path";
 import execa from "execa";
 // import { gzipSync } from "zlib";
 // import { compress } from "brotli";
 import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
+import { existsSync } from "fs";
 
 const args = require('minimist')(process.argv.slice(2));
 async function build() {
@@ -35,7 +36,22 @@ async function build() {
       configObjectFullPath: resolve(__dirname, `../api-extractor.json`),
       packageJsonFullPath: resolve(__dirname, `../packages/${pkg}/package.json`),
     });
-    Extractor.invoke(config);
+    const result = Extractor.invoke(config);
+    if (result.succeeded) {
+      const pkgDir = resolve('./packages', pkg);
+      const globalPath = path.resolve(pkgDir, 'global.d.ts');
+      if (existsSync(globalPath)) {
+        const dtsPath = path.resolve(pkgDir, 'dist/index.d.ts');
+        const existing = await readFile(dtsPath, 'utf-8');
+        await writeFile(dtsPath, '/// <reference path="./global.d.ts"/>' + '\n' + existing);
+        await copyFile(globalPath, path.resolve(pkgDir, 'dist/global.d.ts'));
+      }
+
+      console.log(chalk.bold(chalk.green(`API Extractor completed package ${pkg} successfully.`)));
+    } else {
+      console.error(`API Extractor shutdown with ${result.errorCount} errors and ${result.warningCount} warnings`);
+      process.exit(1);
+    }
   }
 
 }
